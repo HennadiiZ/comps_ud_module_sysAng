b80a79d4a33a5c2214ee78e8095a29f16af62866
Hennadii <82006228+HennadiiZ@users.noreply.github.com>
2022-05-29 14:04:14 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'comps_ud_module_sys';
+}
